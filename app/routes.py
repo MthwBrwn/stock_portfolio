b5a662ca442fd5d8742349aa0067c8cfda6b0842
mthwbrwn <mthwbrwn@gmail.com>
2018-12-05 22:01:43 -0800

    error on wsgi page doesnt recognize app

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,25 @@
 from . import app
+
+# 3rd prty requirements
+from flask import render_template, abort, redirect, url_for, session, g, make_response
+from sqlalchemy.exc import IntegrityError
+
+# API requests
 import requests as req
+from json import JSONDecodeError
+import json
+import os
+
+
+# helpers
 
 def get_stock_info(code):
     return req.get(f"{os.getenv('API_URL')}{code}&APPID={os.getenv('API_KEY')}")
+
+
+# controllers
+
+@app.route('/')
+def home()
+    """ this route is the home page for the stocks app """
+    return 'test'
